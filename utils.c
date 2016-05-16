279efd406ebf238205ce74ad096285ad23e36d96
qiuwunogu <576843731@qq.com>
2016-05-16 09:53:21 +0800

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdio.h>
 /*
  * @dst:have enough space to save 'dir' and 'file'
  * @dir: directory path, not null
@@ -7,18 +9,18 @@
 char *utils_path_combination (char *dst, char *dir, char *file)
 {
 #ifdef PLATFORM_WINDOWS
-  char split = '\';
+  char split = '\\';
 #else //linux
   char split = '/';
 #endif
   int dir_len = strlen (dir);
   
   if (dir[dir_len - 1] == split && file[0] == split)
-    sprintf (dst, "%s %s", dir, file + 1);  
+    sprintf (dst, "%s%s", dir, file + 1);  
   else if (dir[dir_len - 1] != split && file[0] != split)
-    sprintf (dst, "%s %c %s", dir, split,  file + 1);
+    sprintf (dst, "%s%c%s", dir, split,  file);
   else
-    sprintf (dst, "%s %s", dir, file);
-	
+    sprintf (dst, "%s%s", dir, file);
+
   return dst;
 }
